f2a844f89b716c49ff4530f23ccfe0cd709d4fc5
Arisandi Kader <arisandikaderdev@gmail.com>
2023-07-12 20:19:43 +0900

    first commit with sheild

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -6,8 +6,8 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = '';
-    public string $fromName   = '';
+    public string $fromEmail  = 'arisandikader2@gmail.com';
+    public string $fromName   = 'arisandi';
     public string $recipients = '';
 
     /**
